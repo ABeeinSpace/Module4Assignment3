f0375c358fdfa19b663f31c829183ed85a3b921a
Aidan Border <planes4u@outlook.com>
2022-03-18 11:09:33 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+/*ALGORITHM:
+*
+* */
+
+/*NOTES:
+* Should probably mod the object's weights by the weight limit after its randomly generated, so we don't exceed that
+* limit with one item.*/
+
+import java.util.*;
+
+public class Main {
+
+    /* main()
+    * Parameters: String[] args
+    * Returns: N/A
+    * Description: Method to support running/testing of project code
+    * Special: Method gets implicitly called by java at runtime, and only exits at program exit.*/
+    public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+
+
+    }
+
+    /* printList()
+     * Parameters:
+     * Returns: N/A (Void)
+     * Description: Prints the list of items once we generate it.*/
+    private static void printList() {
+
+    }
+    /* generateList()
+     * Parameters:
+     * Returns: N/A (Void)
+     * Description: Generates the list of items, their values, and their weights. Calls */
+    private static void generateList() {
+        Random rng = new Random();
+
+    }
+
+
+}
