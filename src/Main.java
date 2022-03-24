391b4eb7c14c42a6cafa3b7975d094879059ba20
Aidan Border <planes4u@outlook.com>
2022-03-23 22:16:03 -0400

    Changing formatting to match what Doc Lewis wants

--- src/Main.java
+++ src/Main.java
@@ -186,3 +186,47 @@ public class Main {
 
 
 }
+
+class Item {
+    private String itemName;
+    private int weight;
+    private int value;
+
+    public Item(String itemName, int weight, int value) {
+        this.itemName = itemName;
+        this.weight = weight;
+        this.value = value;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        output = output + String.format("%-42s%4s %8d %17d", itemName, "", weight, value);
+        return output;
+    }
+}
+
